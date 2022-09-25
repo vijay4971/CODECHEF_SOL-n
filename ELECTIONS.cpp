8b59eb75568b7eb735b64ca5c68322f9c220588b
Vijay Chaure <98203583+vijay4971@users.noreply.github.com>
2022-09-26 01:24:27 +0530

    Create ELECTIONS.cpp

---
+++ ELECTIONS.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    
+	    if(a+b+c == 101)
+	    {
+	        if(a > 50)
+	        {
+	            cout<<"A"<<endl;
+	        }
+	        else if(b > 50)
+	        {
+	            cout<<"B"<<endl;
+	        }
+	        else if(c > 50)
+	        {
+	            cout<<"C"<<endl;
+	        }
+	        else
+	        {
+	            cout<<"NOTA"<<endl;
+	        }
+	    }
+	}
+	return 0;
+}
