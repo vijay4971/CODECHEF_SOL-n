0feb191fb70ab46ee5a6714952c13dfe5d41e029
Vijay Chaure <98203583+vijay4971@users.noreply.github.com>
2022-09-26 14:21:06 +0530

    Create URCALC.cpp

---
+++ URCALC.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	double a,b;
+	cin>>a>>b;
+	
+	
+	char ch;
+	cin>>ch;
+	
+	switch(ch)
+	{
+	    case '+': 
+	            cout<<(a+b)<<endl;
+	            break;
+	    case '-':
+	            cout<<a-b<<endl;
+	            break;
+	    case '*':
+	            cout<<a*b<<endl;
+	            break;
+	    case '/':
+	            cout<<fixed<<a/b<<endl;
+	            break;
+	}
+	
+	return 0;
+}
