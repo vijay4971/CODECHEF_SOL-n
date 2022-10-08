2138b62f0670be387509d4373439250a9ab5b341
Vijay Chaure <98203583+vijay4971@users.noreply.github.com>
2022-10-08 19:04:02 +0530

    Create PIZZA_BURGER.cpp

---
+++ PIZZA_BURGER.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    
+	    if(y<=x)
+	    {
+	        cout<<"PIZZA"<<endl;
+	    }
+	    else if(z<=x)
+	    {
+	        cout<<"BURGER"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NOTHING"<<endl;
+	    }
+	}
+	return 0;
+}
